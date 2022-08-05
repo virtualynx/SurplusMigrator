f0817464d7773ed1611a7901508806569defe8a7
virtualynx <eryandi.angga.pratama@gmail.com>
2022-08-05 07:04:41 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SurplusMigrator {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
