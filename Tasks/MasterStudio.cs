552d6d37eecab09e16a34725731f939697ae94a7
virtualynx <eryandi.angga.pratama@gmail.com>
2023-01-02 06:51:57 +0700

    add migration for MasterStudio and MasterStudioGroup

---
+++ Tasks/MasterStudio.cs
@@ -0,0 +1,83 @@
+using SurplusMigrator.Libraries;
+using SurplusMigrator.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SurplusMigrator.Tasks {
+    class MasterStudio : _BaseTask {
+        public MasterStudio(DbConnection_[] connections) : base(connections) {
+            sources = new TableInfo[] {
+                new TableInfo() {
+                    connection = connections.Where(a => a.GetDbLoginInfo().dbname == "E_FRM").FirstOrDefault(),
+                    tableName = "master_crewequipmentstudio",
+                    columns = new string[] {
+                        "studio_id",
+                        "studio_name",
+                        "studio_location",
+                        "studio_remark",
+                        "studio_category_id",
+                        "studio_createdby",
+                        "studio_createddate",
+                        "studio_modifiedby",
+                        "studio_modifieddate",
+                        "studio_isdisabled",
+                        "studio_disableby",
+                        "studio_disabledate",
+                    },
+                    ids = new string[] { "studio_id" }
+                }
+            };
+            destinations = new TableInfo[] {
+                new TableInfo() {
+                    connection = connections.Where(a => a.GetDbLoginInfo().dbname == "insosys").FirstOrDefault(),
+                    tableName = "master_studio",
+                    columns = new string[] {
+                        "studioid",
+                        "name",
+                        "address",
+                        "remark",
+                        "studiogroupid",
+                        "created_by",
+                        "created_date",
+                        "is_disabled",
+                        "disabled_by",
+                        "disabled_date",
+                        "modified_by",
+                        "modified_date",
+                    },
+                    ids = new string[] { "studioid" }
+                }
+            };
+        }
+
+        protected override List<RowData<ColumnName, object>> getSourceData(Table[] sourceTables, int batchSize = defaultReadBatchSize) {
+            return sourceTables.Where(a => a.tableName == "master_crewequipmentstudio").FirstOrDefault().getDatas(batchSize);
+        }
+
+        protected override MappedData mapData(List<RowData<ColumnName, object>> inputs) {
+            MappedData result = new MappedData();
+
+            foreach(RowData<ColumnName, object> data in inputs) {
+                result.addData(
+                    "master_studio",
+                    new RowData<ColumnName, object>() {
+                        { "studioid",  data["studio_id"]},
+                        { "name",  data["studio_name"]},
+                        { "address",  data["studio_location"]},
+                        { "remark",  data["studio_remark"]},
+                        { "studiogroupid",  data["studio_category_id"]},
+                        { "created_by", getAuthInfo(data["studio_createdby"], true) },
+                        { "created_date",  data["studio_createddate"]},
+                        { "is_disabled", !Utils.obj2bool(data["studio_isdisabled"]) },
+                        { "disabled_by",  getAuthInfo(data["studio_disableby"]) },
+                        { "disabled_date",  data["studio_disabledate"] },
+                        { "modified_by", getAuthInfo(data["studio_modifiedby"]) },
+                        { "modified_date",  data["studio_modifieddate"]},
+                    }
+                );
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tasks/MasterStudioGroup.cs
@@ -0,0 +1,53 @@
+using SurplusMigrator.Models;
+using System;
+using System.Linq;
+
+namespace SurplusMigrator.Tasks {
+    class MasterStudioGroup : _BaseTask {
+        public MasterStudioGroup(DbConnection_[] connections) : base(connections) {
+            sources = new TableInfo[] {
+            };
+            destinations = new TableInfo[] {
+                new TableInfo() {
+                    connection = connections.Where(a => a.GetDbLoginInfo().dbname == "insosys").FirstOrDefault(),
+                    tableName = "master_studio_group",
+                    columns = new string[] {
+                        "studiogroupid",
+                        "name",
+                        "created_by",
+                        "created_date",
+                        "is_disabled"
+                    },
+                    ids = new string[] { "studiogroupid" }
+                }
+            };
+        }
+
+        protected override MappedData getStaticData() {
+            MappedData result = new MappedData();
+
+            result.addData(
+                "master_studio_group",
+                new RowData<ColumnName, object>() {
+                    { "studiogroupid", 1},
+                    { "name", "Studio 9 & 11"},
+                    { "created_by", DefaultValues.CREATED_BY},
+                    { "created_date", DateTime.Now},
+                    { "is_disabled", false},
+                }
+            );
+            result.addData(
+                "master_studio_group",
+                new RowData<ColumnName, object>() {
+                    { "studiogroupid", 2},
+                    { "name", "Studio Trans7"},
+                    { "created_by", DefaultValues.CREATED_BY},
+                    { "created_date", DateTime.Now},
+                    { "is_disabled", false},
+                }
+            );
+
+            return result;
+        }
+    }
+}
