f49857266c329ef573f9105fe815e8ba363e3e6d
virtualynx <eryandi.angga.pratama@gmail.com>
2023-03-10 09:14:57 +0700

    finalize _FixAdvertiserBrand

--- Tasks.Other/_FixAdvertiserBrand.cs
+++ Tasks.Other/_FixAdvertiserBrand.cs
@@ -1,3 +1,4 @@
+using SurplusMigrator.Exceptions.Gen21;
 using SurplusMigrator.Libraries;
 using SurplusMigrator.Models;
 using System;
@@ -47,73 +48,109 @@ namespace SurplusMigrator.Tasks {
                     MyConsole.Information("Fixing advertiserid and advertiserbrandid in " + tablename + " ... ");
 
                     string[] primaryKeys = QueryUtils.getPrimaryKeys(targetConnection, tablename);
+                    if(primaryKeys.Length == 0 || primaryKeys.Length > 1) {
+                        throw new Exception("Invalid primary-key number: " + primaryKeys.Length);
+                    }
+
+                    var distinctedBrand = QueryUtils.executeQuery(
+                        targetConnection,
+                        //@"
+                        //    select distinct(advertiserbrandid) 
+                        //    from transaction_journal 
+                        //    where 
+                        //        (advertiserid is not null and advertiserid <> '0')
+                        //        and (advertiserbrandid is not null and advertiserbrandid <> '0' and advertiserbrandid ~ '^([0-9]+\.?[0-9]*|\.[0-9]+)$')
+                        //"
+                        @"
+                            select 
+	                            distinct advertiserid, advertiserbrandid, count(1)
+                            from ""<tablename>""
+                            where 
+                                advertiserid is not null and advertiserid <> '0'
+                                and advertiserbrandid is not null and advertiserbrandid <> '0'
+                                and advertiserbrandid ~ '^([0-9]+\.?[0-9]*|\.[0-9]+)$'
+                            group by advertiserid, advertiserbrandid
+                        "
+                        .Replace("<tablename>", tablename)
+                    );
+
                     int updatedCount = 0;
-
-                    var distinctedBrand = QueryUtils.executeQuery(targetConnection, "select distinct(advertiserbrandid) from transaction_journal where advertiserbrandid is not null");
-
                     foreach(var dbrow in distinctedBrand) {
+                        string advertiserid = dbrow["advertiserid"].ToString();
                         string advertiserbrandid = dbrow["advertiserbrandid"].ToString();
 
                         string queryCount = @"
-                                select count(1) from ""<target_schema>"".""<tablename>""
-                                where ""advertiserbrandid"" = <oldvalue>
+                                select count(1) from ""<tablename>""
+                                where 
+                                    advertiserid = @old_advertiserid
+                                    and advertiserbrandid = @old_advertiserbrandid
                                 ;
-                            ";
-                        queryCount = queryCount.Replace("<target_schema>", targetConnection.GetDbLoginInfo().schema);
-                        queryCount = queryCount.Replace("<tablename>", tablename);
-                        queryCount = queryCount.Replace("<oldvalue>", QueryUtils.getInsertArg(advertiserbrandid));
-                        var rsCount = QueryUtils.executeQuery(targetConnection, queryCount);
+                            "
+                            .Replace("<tablename>", tablename)
+                        ;
+
+                        var parameters = new Dictionary<string, object> {
+                            { "@old_advertiserid", advertiserid },
+                            { "@old_advertiserbrandid", advertiserbrandid }
+                        };
+
+                        var rsCount = QueryUtils.executeQuery(targetConnection, queryCount, parameters);
                         int dataCount = Utils.obj2int(rsCount.First()["count"]);
 
                         string querySelect = @"
-                                select <select_columns> from ""<target_schema>"".""<tablename>""
+                                select ""<select_column>"" from ""<tablename>""
                                 where 
-                                    advertiserid is not null
-                                    and advertiserbrandid = <oldvalue>
-                                LIMIT <batch_size>
+                                    advertiserid = @old_advertiserid
+                                    and advertiserbrandid = @old_advertiserbrandid
                                 ;
-                            ";
-                        List<string> selectColumns = new List<string>() { "advertiserid" };
-                        selectColumns.AddRange(primaryKeys);
-                        querySelect = querySelect.Replace("<select_columns>", "\"" + String.Join("\",\"", selectColumns) + "\"");
-                        querySelect = querySelect.Replace("<target_schema>", targetConnection.GetDbLoginInfo().schema);
-                        querySelect = querySelect.Replace("<tablename>", tablename);
-                        querySelect = querySelect.Replace("<oldvalue>", QueryUtils.getInsertArg(advertiserbrandid));
-                        querySelect = querySelect.Replace("<batch_size>", DEFAULT_BATCH_SIZE.ToString());
+                            "
+                            .Replace("<select_column>", primaryKeys[0])
+                            .Replace("<tablename>", tablename)
+                        ;
 
-                        int processedCount = 0;
-                        RowData<ColumnName, object>[] batchDatas;
-                        while((batchDatas = QueryUtils.executeQuery(targetConnection, querySelect)).Length > 0) {
-                            var distinctedAdvertiserId = (from row in batchDatas select row["advertiserid"].ToString()).Distinct().ToArray();
-                            string advertiserid = dbrow["advertiserid"].ToString();
-                            (string newAdvertiserId, string newBrandId) = gen21.getAdvertiserBrandId(distinctedAdvertiserId[0], advertiserbrandid);
+                        string newAdvertiserId;
+                        string newBrandId;
+                        try {
+                            (newAdvertiserId, newBrandId) = gen21.getAdvertiserBrandId(advertiserid, advertiserbrandid);
+                        } catch(MissingAdvertiserBrandException) {
+                            MyConsole.Warning("Missing Advertiser-Brand: AdvertiserId["+ advertiserid + "] BrandId["+ advertiserbrandid + "]");
+                            continue;
+                        }
+
+                        var data = QueryUtils.executeQuery(targetConnection, querySelect, parameters);
+                        if(data.Length > 0) {
+                            string[] filterValues = data.Select(a => Utils.obj2str(a[primaryKeys[0]])).ToArray();
 
                             string queryUpdate = @"
-                                    update ""<target_schema>"".""<tablename>"" 
+                                    update ""<tablename>"" 
                                     set 
-                                        advertiserid = <newadvertiserid>
-                                        advertiserbrandid = <newbrandid>
-                                    where (<filter_columns>) IN (<filter_values>);
-                                ";
-                            queryUpdate = queryUpdate.Replace("<target_schema>", targetConnection.GetDbLoginInfo().schema);
-                            queryUpdate = queryUpdate.Replace("<tablename>", tablename);
-                            queryUpdate = queryUpdate.Replace("<newadvertiserid>", QueryUtils.getInsertArg(newAdvertiserId));
-                            queryUpdate = queryUpdate.Replace("<newbrandid>", QueryUtils.getInsertArg(newBrandId));
-                            queryUpdate = queryUpdate.Replace("<filter_columns>", "\"" + String.Join("\",\"", primaryKeys) + "\"");
+                                        advertiserid = @newadvertiserid, advertiserbrandid = @newbrandid
+                                    where ""<filter_column>"" IN @filter_values;
+                                "
+                                .Replace("<tablename>", tablename)
+                                .Replace("<filter_column>", primaryKeys[0])
+                            ;
 
-                            List<string> filterValues = new List<string>();
-                            foreach(var row in batchDatas) {
-                                List<string> filterValArray = new List<string>();
-                                foreach(var pk in primaryKeys) {
-                                    filterValArray.Add(QueryUtils.getInsertArg(row[pk]));
+                            var updateResult = QueryUtils.executeQuery(
+                                targetConnection, 
+                                queryUpdate, 
+                                new Dictionary<string, object> {
+                                    { "@newadvertiserid", newAdvertiserId },
+                                    { "@newbrandid", newBrandId },
+                                    { "@filter_values", filterValues }
                                 }
-                                filterValues.Add("(" + String.Join(",", filterValArray) + ")");
-                            }
-                            queryUpdate = queryUpdate.Replace("<filter_values>", String.Join(",", filterValues));
-                            QueryUtils.executeQuery(targetConnection, queryUpdate);
-                            processedCount += batchDatas.Length;
-                            MyConsole.EraseLine();
-                            MyConsole.Write(processedCount + "/" + dataCount + " data processed ... ");
+                            );
+
+                            updatedCount += data.Length;
+
+                            MyConsole.Information(
+                                "Update [advertiser, advertiserbrand]: [@old_advertiserid, @old_advertiserbrandid] => [@newadvertiserid, @newbrandid] (@datacount data)"
+                                .Replace("@old_advertiserid", advertiserid)
+                                .Replace("@old_advertiserbrandid", advertiserbrandid)
+                                .Replace("@newadvertiserid", newAdvertiserId)
+                                .Replace("@newbrandid", newBrandId)
+                                .Replace("@datacount", data.Length.ToString())
+                            );
                         }
                     }
 
